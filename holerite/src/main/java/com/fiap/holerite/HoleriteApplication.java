fe0a9013f83abd9b6867b6c75679bd041d63b29d
Vitor Avellar <125935979+VitorAvellar@users.noreply.github.com>
2024-03-13 22:25:11 -0300

    Add files via upload

---
+++ holerite/src/main/java/com/fiap/holerite/HoleriteApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.holerite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoleriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoleriteApplication.class, args);
+	}
+
+}
---
+++ holerite/src/test/java/com/fiap/holerite/HoleriteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.holerite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoleriteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
